9349fb6473433c015b9844851d9c3de38186a09f
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2022-01-15 16:29:17 -0700

    Add and modify some files

--- project/launcher/views.py
+++ project/launcher/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from launcher.models import Housing
+from .filters import HousingFilter
+import csv
 # Create your views here.
 
 
@@ -14,4 +16,40 @@ def index(request):
     }
 
     # send request to the index page with the housing data passed in
+    return render(request, 'index.html', context)
+
+# Try to filter stuff here (MVP) 
+def search(request):
+    # with open('/Users/cheetey/Documents/Winter2022/W/project/launcher/test_model.csv', 'r') as file:
+    #     reader = csv.reader(file)
+    #     for row in reader:
+    #         print(row)
+    distance = request.GET.get("distance")
+    print("Distance provided: " + distance)
+
+    housing_list_from_db = Housing.objects.all()
+    
+    # Loop thru the dataset and save those shelters that are within the range into "within_dist"
+    within_distance_shelter_names = []
+
+    # Distance parameter is provided
+    if distance:
+        with open('/Users/cheetey/Documents/Winter2022/W/project/launcher/test_model.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(row[3])
+                if int(row[3]) <= int(distance):
+                    # print("row: " + row[3] " and distance: " + distance)
+                    within_distance_shelter_names.append(row[0])
+
+    # Distance parameter is not provided
+    else:
+        within_distance_shelter_names = housing_list_from_db
+    print(within_distance_shelter_names)
+
+    housing_filter = HousingFilter(request.GET, queryset=housing_list_from_db)
+    context = {
+        'filter': housing_filter,
+        'distance': within_distance_shelter_names
+    }
     return render(request, 'index.html', context)
