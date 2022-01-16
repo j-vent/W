c8918efa37091bd78c1f34a5b37957e186cc0098
Justine Ventura <jvent@venturazure.onmicrosoft.com>
2022-01-15 20:20:00 -0700

    stuff idk

--- project/launcher/models.py
+++ project/launcher/models.py
@@ -3,20 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Housing(models.Model):
+    
     organization = models.CharField(max_length=100)
     shelter_name = models.CharField(max_length=100)
     shelter_type = models.CharField(max_length=100)
     address = models.TextField(max_length=100)
-    latitude = models.DecimalField(max_digits=10)
-    longitude = models.DecimalField(max_digits=10)
+    latitude = models.DecimalField(max_digits=10,decimal_places=5, default=0)
+    longitude = models.DecimalField(max_digits=10, decimal_places=5, default=0)
     capacity = models.IntegerField()
-    
-    lgbtq2s_friendly = models.BooleanField()
-    wheelchair_accessible = models.BooleanField()
-    public_transit_accessible = models.BooleanField()
-    women_only = models.BooleanField()
-    food_provided = models.BooleanField()
-    showers_provided = models.BooleanField()
+    # TODO: rewrite as enum
+    # 0 = No, 1 = Yes, 2 = No Information Available
+    lgbtq2s_friendly = models.IntegerField(default=2)
+    wheelchair_accessible = models.IntegerField(default=2)
+    public_transit_accessible = models.IntegerField(default=2)
+    women_only = models.IntegerField(default=2)
+    food_provided = models.IntegerField(default=2)
+    showers_provided =models.IntegerField(default=2)
 
     # TODO: add ratings for security, comfort, etc.
     rating = models.FloatField()
