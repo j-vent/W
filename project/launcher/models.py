22d4177766f5450fea3e1bbc6f97a96f1c1a9f4b
Justine Ventura <jvent@venturazure.onmicrosoft.com>
2022-01-15 19:45:49 -0700

    added db fields

--- project/launcher/models.py
+++ project/launcher/models.py
@@ -7,7 +7,18 @@ class Housing(models.Model):
     shelter_name = models.CharField(max_length=100)
     shelter_type = models.CharField(max_length=100)
     address = models.TextField(max_length=100)
+    latitude = models.DecimalField(max_digits=10)
+    longitude = models.DecimalField(max_digits=10)
     capacity = models.IntegerField()
+    
+    lgbtq2s_friendly = models.BooleanField()
+    wheelchair_accessible = models.BooleanField()
+    public_transit_accessible = models.BooleanField()
+    women_only = models.BooleanField()
+    food_provided = models.BooleanField()
+    showers_provided = models.BooleanField()
+
+    # TODO: add ratings for security, comfort, etc.
     rating = models.FloatField()
-    # add other fields like LGBTQ-friendly, pet-friendly, showers provided, food provided, etc.
-    # lgbtq_friendly = models.BooleanField()
+
+    
